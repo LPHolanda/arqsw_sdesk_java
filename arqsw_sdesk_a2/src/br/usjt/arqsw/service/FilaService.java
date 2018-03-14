a67558654edfb7fbf9f2eebd51d5ea59e044ba59
arqdsis <arqdsis@mocli10228.animaedu.intranet>
2018-03-13 22:07:23 -0300

    aula2 - feito em aula(falta login)

---
+++ arqsw_sdesk_a2/src/br/usjt/arqsw/service/FilaService.java
@@ -0,0 +1,35 @@
+package br.usjt.arqsw.service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.usjt.arqsw.dao.FilaDAO;
+import br.usjt.arqsw.entity.Fila;
+
+/**
+ * 
+ * @author Leandro Pinheiro de Holanda 816113762
+ *
+ */
+
+@Service
+public class FilaService {
+	
+	private FilaDAO dao;
+	
+	@Autowired
+	public FilaService(FilaDAO dao) {
+		this.dao = dao;
+	}
+	
+	public ArrayList<Fila> listarFilas() throws IOException{
+		return dao.listarFilas();
+	}
+	
+	public Fila carregar(int id) throws IOException{
+		return dao.carregar(id);
+	}
+}
